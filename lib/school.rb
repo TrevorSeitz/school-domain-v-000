505e97bbdd9271cf4335e695387d66ba2485e685
Trevor Seitz <trevinla@gmail.com>
2018-03-16 23:03:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,27 +22,9 @@ class School
 
   def sort
     # binding.pry
-    roster.sort { |level, student| level }
+    roster.sort_by { |level, student| level }
     roster
   end
 end
 
 school = School.new("Bayside High School")
-
-# class Shoe
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brand
-#
-#   BRANDS = []
-#
-#   def initialize(brand)
-#     @brand = brand
-#     BRANDS.push(brand) unless BRANDS.include?(brand)
-#   end
-#
-#   def cobble
-#     self.condition = "new"
-#     puts "Your shoe is as good as new!"
-#   end
-#
-# end
