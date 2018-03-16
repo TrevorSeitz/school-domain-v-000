9b4c84bf2f7950383e5edbdce2cc59fad59387a7
Trevor Seitz <trevinla@gmail.com>
2018-03-16 22:19:44 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ class School
 
   def add_student(student, grade)
     # @student = student
-    roster.merge! (grade, student)
+    roster[student] =>  grade
     # roster.push(@student) unless roster.include?(@student)
   end
 
