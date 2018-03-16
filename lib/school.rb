78040a448aff9c8192e6d93e24a78aa8d39d1a00
Trevor Seitz <trevinla@gmail.com>
2018-03-16 21:50:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 # code here!
-class School(school_name)
-  attr_accessor 
+class School
+  attr_accessor
   attr_reader :school_name
 
-  def school_name
+  def initialize(school_name)
     @school_name = school_name
   end
 end
